24e9795f8e93f70b35498078f2855a975012d5e7
rongmingye <1164898915@qq.com>
2021-12-23 22:26:19 +0800

    feat: 引入sass, 首页groud

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import 'antd-mobile/dist/antd-mobile.css';
-import './index.css';
+import './assets/css/index.scss'
+import './index.scss';
 
 ReactDOM.render(
   <React.StrictMode>
