16d84125b62ee4546d4e7085262a510a4f3d913b
rongmingye <1164898915@qq.com>
2021-12-27 00:04:35 +0800

    feat: 地图功能，引入百度地图

--- src/pages/Index/search.js
+++ src/pages/Index/search.js
@@ -1,8 +1,25 @@
 import { useNavigate } from 'react-router-dom'
 import { Flex } from 'antd-mobile';
+import { useState, useEffect } from 'react/cjs/react.development';
+import { getCurrentCity } from '../../utils';
 
 export default function Search() {
     const navigate = useNavigate()
+    const [cityName, setCityName] = useState('上海')
+
+    // h5 api 获取当前定位
+    navigator.geolocation.getCurrentPosition(function(position) {
+      getLocationName(position.coords.latitude, position.coords.longitude);
+    })
+
+    const getLocationName = (lat, long) => {
+
+    }
+
+    useEffect(async () => {
+      const curCity = await getCurrentCity()
+      setCityName(curCity.label || '上海')
+    }, [])
 
     const handleLocation = () => {
         navigate('/citylist')
@@ -20,7 +37,7 @@ export default function Search() {
         <Flex className='search-box'>
           <Flex className="search">
             <div className='location' onClick={() => handleLocation()}>
-              <span className='name'>上海</span>
+              <span className='name'>{cityName}</span>
               <i className='iconfont icon-arrow'></i>
             </div>
             <div className='form' onClick={()=> handleForm()}>
--- src/pages/Map/index.js
+++ src/pages/Map/index.js
@@ -1,4 +1,18 @@
+import axios from 'axios'
+import { useState, useEffect } from 'react'
+import './index.scss'
 
 export default function Map() {
-  return (<div>map</div>)
+
+  useEffect(() => {
+    const map = new window.BMapGL.Map("container")
+    const point = new window.BMapGL.Point(116.404, 39.915)
+    map.centerAndZoom(point, 6)
+  }, [])
+
+  return (
+    <div className='map'>
+      <div id="container"></div>
+    </div>
+  )
 }
